dae2a36d5ff6f54766bdc7fae0d9f767a12be42e
Hui Shen Sam <hui-shen.sam.yh@renesas.com>
2025-01-31 16:02:28 +0800

    Init commit

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -87,6 +87,17 @@ def authorized():
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = None
+        try:
+            msal_client = _build_msal_app(cache=cache)
+            result = msal_client.acquire_token_by_authorization_code(
+                request.args['code'],
+                scopes=Config.SCOPE,
+                redirect_uri=url_for('authorized', _external=True)
+            )
+        except Exception as exc:
+            app.logger.exception("Unable to authenticate user")
+            error = str(exc)
+            result = {'error': error, 'error_description': 'Function not implemented.'}
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -95,6 +106,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
+        app.logger.info("User logged in successful")
     return redirect(url_for('home'))
 
 @app.route('/logout')
@@ -112,17 +124,30 @@ def logout():
 
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
-    return None
+    client = msal.ConfidentialClientApplication(
+        Config.CLIENT_ID,
+        client_credential=Config.CLIENT_SECRET,
+        authority=authority or Config.AUTHORITY,
+        token_cache=cache
+    )
+    return client
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _scheme='https')
+    )
--- WRITEUP.md
+++ WRITEUP.md
@@ -7,6 +7,17 @@
 - *Choose the appropriate solution (VM or App Service) for deploying the app*
 - *Justify your choice*
 
+I would choose an App Service as there is not much customization needed for the
+application. In terms of scalability, as the application is light weight, the scalability options
+under App Service is sufficient to host our application. For availability, App Service provides
+horizontal scaling to ensure services are running even if one of the nodes is down. App 
+Service also supports CI/CD from git, which is suitable as we are developing this code using on github.
+
 ### Assess app changes that would change your decision.
 
 *Detail how the app and any other needs would have to change for you to change your decision in the last section.* 
+
+If there is a requirement to customize the application (i.e. security, features) at the OS level, 
+we might have to switch to VM as it allows better customization, and if compute requirements increases
+with the increase in concurrent users, then switching to VM is better due to its higher scalability
+than App Services.
